7825f087fe7cef73431a089aad51eceffd3b9cfb
AshDragon55 <ashert1000@gmail.com>
2023-03-02 13:45:06 -0400

    image added

--- imports/listProfiles.js
+++ imports/listProfiles.js
@@ -7,11 +7,19 @@ Template.listProfiles.helpers({
 })
 
 Template.listProfiles.events({
-    'click .js-view'(){
+    'click .js-view'() {
         let that = this
         const viewModal = new bootstrap.Modal('#viewModal', {})
-        console.warn("viewing", that._id)
+        console.warn("viewing", that)
+        document.querySelector('#viewProfile').src = that.profile
+        document.querySelector('#viewName').innerHTML = `${that.first} ${that.surname}`
+        document.querySelector('#viewAge').innerHTML = that.age
+        document.querySelector('#viewLocation').innerHTML = `location: ${that.address}`
         viewModal.show()
     },
-
+    'click .js-delete'() {
+        let that = this
+        console.debug("deleting", that._id)
+        socialdb.remove({ "_id": that._id })
+    }
 })
--- imports/viewProfiles.js
+++ imports/viewProfiles.js
@@ -1,5 +1,12 @@
-Template.viewProfiles.helpers({
-    profiles(){
-        return socialdb.find()
-    }
+Template.viewProfiles.events({
 })
+
+
+// Template.viewProfiles.helpers({
+//     profiles(){
+//         return socialdb.find()
+//     }
+// })
+
+// import * as bootstrap from 'bootstrap'
+
